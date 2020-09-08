549c2b1bfd3b1861f581c3781a27f0fbea260edd
Taylor Cannon <taylorcannon@Taylors-MacBook-Pro.local>
2020-09-08 10:52:27 -0500

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="Header">React Trello Clone</header>
+    </div>
+  );
+}
+
+export default App;
