6271a9c5b5d31740cdaf4f197d0528578d1fec9b
Taylor Cannon <taylorcannon@Taylors-MacBook-Pro.local>
2020-09-08 11:24:17 -0500

    added lists and cards reducers

--- src/store.js
+++ src/store.js
@@ -22,3 +22,107 @@ const board = (state = { lists: [] }, action) => {
         return state;
     }
   };
+
+  /* LISTS REDUCER */
+  const listsById = (state = {}, action) => {
+    switch (action.type) {
+      case "ADD_LIST": {
+        const { listId, listTitle } = action.payload;
+        return {
+          ...state,
+          [listId]: { _id: listId, title: listTitle, cards: [] }
+        };
+      }
+      case "CHANGE_LIST_TITLE": {
+        const { listId, listTitle } = action.payload;
+        return {
+          ...state,
+          [listId]: { ...state[listId], title: listTitle }
+        };
+      }
+      case "DELETE_LIST": {
+        const { listId } = action.payload;
+        const { [listId]: deletedList, ...restOfLists } = state;
+        return restOfLists;
+      }
+      case "ADD_CARD": {
+        const { listId, cardId } = action.payload;
+        return {
+          ...state,
+          [listId]: { ...state[listId], cards: [...state[listId].cards, cardId] }
+        };
+      }
+      case "MOVE_CARD": {
+        const {
+          oldCardIndex,
+          newCardIndex,
+          sourceListId,
+          destListId
+        } = action.payload;
+        // Move within the same list
+        if (sourceListId === destListId) {
+          const newCards = Array.from(state[sourceListId].cards);
+          const [removedCard] = newCards.splice(oldCardIndex, 1);
+          newCards.splice(newCardIndex, 0, removedCard);
+          return {
+            ...state,
+            [sourceListId]: { ...state[sourceListId], cards: newCards }
+          };
+        }
+        // Move card from one list to another
+        const sourceCards = Array.from(state[sourceListId].cards);
+        const [removedCard] = sourceCards.splice(oldCardIndex, 1);
+        const destinationCards = Array.from(state[destListId].cards);
+        destinationCards.splice(newCardIndex, 0, removedCard);
+        return {
+          ...state,
+          [sourceListId]: { ...state[sourceListId], cards: sourceCards },
+          [destListId]: { ...state[destListId], cards: destinationCards }
+        };
+      }
+      case "DELETE_CARD": {
+        const { cardId: deletedCardId, listId } = action.payload;
+        const filterDeleted = cardId => cardId !== deletedCardId;
+        return {
+          ...state,
+          [listId]: {
+            ...state[listId],
+            cards: state[listId].cards.filter(filterDeleted)
+          }
+        };
+      }
+      default:
+        return state;
+    }
+  };
+
+  /* CARDS REDUCER */
+  const cardsById = (state = {}, action) => {
+    switch (action.type) {
+      case "ADD_CARD": {
+        const { cardText, cardId } = action.payload;
+        return { ...state, [cardId]: { text: cardText, _id: cardId } };
+      }
+      case "CHANGE_CARD_TEXT": {
+        const { cardText, cardId } = action.payload;
+        return { ...state, [cardId]: { ...state[cardId], text: cardText } };
+      }
+      case "DELETE_CARD": {
+        const { cardId } = action.payload;
+        const { [cardId]: deletedCard, ...restOfCards } = state;
+        return restOfCards;
+      }
+      // Find every card from the deleted list and remove it
+      case "DELETE_LIST": {
+        const { cards: cardIds } = action.payload;
+        return Object.keys(state)
+          .filter(cardId => !cardIds.includes(cardId))
+          .reduce(
+            (newState, cardId) => ({ ...newState, [cardId]: state[cardId] }),
+            {}
+          );
+      }
+      default:
+        return state;
+    }
+  };
